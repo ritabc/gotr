767eabc90229eb4317cee9e0ba2b44d6d4163c01
Rita Bennett-Chew <rita.bennett-chew@spirent.com>
2021-02-11 17:21:58 -0500

    through 019-reflection part 2

---
+++ 019-reflection/part-2/ex1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// Get Type using Printf
+func main() {
+	var x interface{}
+	x = 3.13
+	fmt.Printf("x: type = %T, value %v\n", x, x)
+	goo := x
+	fmt.Printf("goo: type = %T, value %v\n", goo, goo)
+
+	x = &struct{ name string }{}
+	fmt.Printf("x: type = %T, value %v\n", x, x)
+	hoo := x
+	fmt.Printf("hoo: type = %T, value %v\n", hoo, hoo)
+
+}
